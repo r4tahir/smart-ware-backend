40012616beed62e7e7a91d8c939f5041bb6a3202
unknown <rtahir0786@gmail.com>
2023-11-12 12:15:37 -0500

    first

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,26 +1,28 @@
 const { MongoClient } = require("mongodb");
 const Db = process.env.ATLAS_URI;
 const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
- 
-var _db;
- 
+
+let _db;
+
 module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("employees");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      return callback(err);
-         });
-  },
-  
-  getDb: function () {
-    return _db;
-  },
+    connectToServer: async function () {
+        try {
+            const db = await client.connect();
+            // Verify we got a good "db" object
+            if (db) {
+                _db = db.db("employees");
+                console.log("Successfully connected to MongoDB.");
+            }
+            return _db;
+        } catch (err) {
+            throw err;
+        }
+    },
+
+    getDb: function () {
+        return _db;
+    },
 };
