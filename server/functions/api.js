8fb39fc43ebf5b7bbc8a06094bd252cc6e5b031d
Masum <masumhsn@gmail.com>
2023-11-14 00:18:28 -0500

    test

--- server/functions/api.js
+++ server/functions/api.js
@@ -1,7 +1,11 @@
 // YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 
-import express, { Router } from "express";
-import serverless from "serverless-http";
+// import express, { Router } from "express";
+// import serverless from "serverless-http";
+
+const express = require("express");
+const serverless = require("serverless-http");
+const { Router } = require("express");
 
 const api = express();
 
@@ -10,4 +14,18 @@ router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
 
-export const handler = serverless(api);
+// export const handler = serverless(api);
+module.exports.handler = serverless(api);
+
+
+// const express = require("express");
+// const serverless = require("serverless-http");
+// const { Router } = require("express");
+
+// const api = express();
+// const router = Router();
+
+// router.get("/hello", (req, res) => res.send("Hello World!"));
+// api.use("/api/", router);
+
+// module.exports.handler = serverless(api);
