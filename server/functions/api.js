2709f80218b00ad3990af21ab77a1420592244e8
Masum <masumhsn@gmail.com>
2023-11-16 17:40:05 -0500

    enable cors

--- server/functions/api.js
+++ server/functions/api.js
@@ -11,6 +11,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(express.json());
 app.use(require(".././routes/record")); // Adjust the path based on your file structure
 
