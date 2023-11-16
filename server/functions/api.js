7bc5fd7efa78b7d16707ea78857831578e4c1fd1
Masum <masumhsn@gmail.com>
2023-11-16 12:20:39 -0500

    deploy

--- server/functions/api.js
+++ server/functions/api.js
@@ -1,31 +1,74 @@
-// YOUR_BASE_DIRECTORY/netlify/functions/api.ts
+// // // // YOUR_BASE_DIRECTORY/netlify/functions/api.ts
 
-// import express, { Router } from "express";
-// import serverless from "serverless-http";
+// // // // import express, { Router } from "express";
+// // // // import serverless from "serverless-http";
 
-const express = require("express");
-const serverless = require("serverless-http");
-const { Router } = require("express");
+// // // const express = require("express");
+// // // const serverless = require("serverless-http");
+// // // const { Router } = require("express");
 
-const api = express();
+// // // const api = express();
 
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+// // // const router = Router();
+// // // router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/api/", router);
+// // // // api.use("/api/", router);
+// // // api.use(router);
 
-// export const handler = serverless(api);
-module.exports.handler = serverless(api);
+// // // // export const handler = serverless(api);
+// // // module.exports.handler = serverless(api);
+
+
+// // // // const express = require("express");
+// // // // const serverless = require("serverless-http");
+// // // // const { Router } = require("express");
+
+// // // // const api = express();
+// // // // const router = Router();
+
+// // // // router.get("/hello", (req, res) => res.send("Hello World!"));
+// // // // api.use("/api/", router);
+
+// // // // module.exports.handler = serverless(api);
+
+
+
+// // const express = require("express");
+// // const serverless = require("serverless-http");
+// // const { Router } = require("express");
+
+// // const api = express();
+// // const router = Router();
+
+// // router.get("/hello", (req, res) => res.send("Hello World!"));
+
+// // api.use("/api", router); // Updated the route here
+
+// // module.exports.handler = serverless(api);
+
+
 
 
 // const express = require("express");
 // const serverless = require("serverless-http");
-// const { Router } = require("express");
 
-// const api = express();
-// const router = Router();
+// const app = express();
 
-// router.get("/hello", (req, res) => res.send("Hello World!"));
-// api.use("/api/", router);
+// app.get("/hello", (req, res) => {
+//   res.status(200).json({ message: "Hello World!" });
+// });
 
-// module.exports.handler = serverless(api);
+// module.exports.handler = serverless(app);
+
+
+const express = require("express");
+const serverless = require("serverless-http");
+
+const app = express();
+
+app.get("/.netlify/functions/api/hello", (req, res) => {
+  res.status(200).json({ message: "Hello World!" });
+});
+
+// Wrap your app in serverless function
+module.exports.handler = serverless(app);
